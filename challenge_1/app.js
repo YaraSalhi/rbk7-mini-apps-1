c047be36e3d82dca0fb3ae0c407a29e01afaad7f
YaraSalhi <yara.salhi@outlook.sa>
2019-10-29 09:06:18 -0700

    xo

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,60 @@
+	var flag= true;
+	var counter = 0;
+	debugger;
+$('#table').click(function(){
+	if($('.cell').text()== "" && flag){
+		if((counter%2===0)){
+			$('.cell').append("X");
+			flag=false;
+		}
+		else{
+		$('.cell').append("O");
+
+		}
+		counter++;
+		}
+});
+
+function winner(){
+	var cell1=$("#table tr:nth-child(1) td:nth-child(1)").text();
+	var cell2=$("#table tr:nth-child(1) td:nth-child(2)").text();
+	var cell3=$("#table tr:nth-child(1) td:nth-child(3)").text();
+	var cell4=$("#table tr:nth-child(2) td:nth-child(1)").text();
+	var cell5=$("#table tr:nth-child(2) td:nth-child(2)").text();
+	var cell6=$("#table tr:nth-child(2) td:nth-child(3)").text();
+	var cell7=$("#table tr:nth-child(3) td:nth-child(1)").text();
+	var cell8=$("#table tr:nth-child(3) td:nth-child(2)").text();
+	var cell9=$("#table tr:nth-child(3) td:nth-child(1)").text();
+
+if((cell1===cell2) && (cell2===cell3)){
+	return cell3;
+
+}else if((cell4===cell5) &&(cell5===cell6)){
+	return cell6;
+
+}else if((cell7===cell8) &&(cell8===cell69)){
+	return cell9;
+
+}else if((cell1===cell4) &&(cell4===cell7)){
+	return cell7;
+
+}else if((cell2===cell5) &&(cell5===cell8)){
+	return cell8;
+
+}else if((cell3===cell6) &&(cell6===cell9)){
+	return cell9;
+
+}else if((cell3===cell5) &&(cell5===cell7)){
+	return cell7;
+
+	}else if((cell1===cell5) &&(cell5===cell9)){
+	return cell9;
+}
+return -1;
+}
+});
 
 
+$('#btn').click(function(){
+			$('.cell').remove();
+        })
